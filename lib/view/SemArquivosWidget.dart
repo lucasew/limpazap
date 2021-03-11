77efcf22737a79583ee00564845cd4cbb46ef9e8
lucasew <lucas59356@gmail.com>
2021-03-11 20:28:52 -0300

    when no files now it show a green tick
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lib/view/SemArquivosWidget.dart
+++ lib/view/SemArquivosWidget.dart
@@ -4,7 +4,7 @@ import '../controller/SnackbarController.dart';
 class SemArquivosWidget extends StatelessWidget {
   Widget build(BuildContext ctx) {
     return Center(
-        child: Icon(Icons.done_sharp, size: 250)
+        child: Icon(Icons.done_sharp, size: 250, color: Colors.green)
     );
   }
 }
