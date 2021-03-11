c2c03978852d14981d45fab103a404c46839448f
lucasew <lucas59356@gmail.com>
2021-03-11 14:16:15 -0300

    SemArquivosWidget: show icon instead of text
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- lib/view/SemArquivosWidget.dart
+++ lib/view/SemArquivosWidget.dart
@@ -2,18 +2,9 @@ import 'package:flutter/material.dart';
 import '../controller/SnackbarController.dart';
 
 class SemArquivosWidget extends StatelessWidget {
-  mostrarInfo(BuildContext ctx) async {
-    SnackbarController(
-            ctx, SnackBar(content: Text("Nenhum arquivo foi encontrado!")))
-        .show();
-  }
-
   Widget build(BuildContext ctx) {
-    mostrarInfo(ctx);
     return Center(
-        child: Text(
-            "Nenhum arquivo encontrado! Toque em atualizar para tentar novamente!",
-            style: TextStyle(fontSize: 36),
-            textAlign: TextAlign.center));
+        child: Icon(Icons.done_sharp, size: 250)
+    );
   }
 }
